3a038f239b545c63da29b593276fe768f960f632
Nelly <nkhmeleva@yahoo.com>
2020-01-07 00:32:41 -0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+//decorator,
+//selector is used in 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'nelly-portfolio';
+ name = 'nelly-portfolio';
 }
+  
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProfileComponent } from './profile/profile.component';
+import { NavbarComponent } from './profile/navbar/navbar.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { AboutComponent } from './profile/about/about.component';
+import { SkillsComponent } from './profile/skills/skills.component';
+import { ProjectsComponent } from './profile/projects/projects.component';
+import { BlogsComponent } from './profile/blogs/blogs.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    ProfileComponent,
+    NavbarComponent,
+    AboutComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    BlogsComponent
+    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule.forRoot(),
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
